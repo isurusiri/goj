a02a6dbb4bce2079e4ab18b11cf98389bda52706
isurusiri <isuruanatomy@gmail.com>
2020-11-10 21:15:43 +0530

    parser: array parsing

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,9 @@
 package parser
+
+// ParseArray iterates over the inputTokens and see if it
+// starts with a left bracket and finishes with a right
+// bracket with commas in between.
+// Params: inputTokens - a map containing a dynamic set of
+//                       values tokenized from the JSON.
+// Returns:
+func ParseArray(inputTokens map[int]interface{}) {}
